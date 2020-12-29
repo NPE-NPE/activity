88f11f0897a428bf81ec4dc4b7053348822da06a
NUL-git <64687067+NUL-git@users.noreply.github.com>
2020-12-29 20:35:09 +0900

    Create 2_3..cpp

---
+++ c++/Atcoder/arisa_compro/2_3..cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  
+  if (S == "yahaa" || S == "yahbb" || S == "yahcc" || S == "yahdd"
+      || S == "yahee" || S == "yahff" || S == "yahgg" || S == "yahhh"
+      || S == "yahii" || S == "yahjj" || S == "yahkk" || S == "yahll"
+      || S == "yahmm" || S == "yahnn" || S == "yahoo" || S == "yahpp"
+      || S == "yahqq" || S == "yahrr" || S == "yahss" || S == "yahtt"
+      || S == "yahuu" || S == "yahvv" || S == "yahww" || S == "yahxx"
+      || S == "yahyy" || S == "yahzz") {
+    cout << "YES" << endl;
+  }
+  //推奨コード
+  //if(S.substr(0, 3) == "yah" && S[3] == S[4])
+  else {
+    cout << "NO" << endl;
+  }
+}

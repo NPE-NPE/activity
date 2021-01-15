f77abb7c8a69df6b15aa8388777a3d7417795328
NUL-git <64687067+NUL-git@users.noreply.github.com>
2021-01-15 21:29:32 +0900

    Update 3_6.cpp

--- c++/Atcoder/arisa_compro/3_6.cpp
+++ c++/Atcoder/arisa_compro/3_6.cpp
@@ -21,3 +21,22 @@ int main() {
   int sum = accumulate(l.begin(), l.end(), 0);
   cout << sum << endl;
 }
+
+//本書のコード
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N, K;
+  cin >> N >> K;
+  
+  vector<int> l(N);
+ 
+  for (int i = 0; i < N; i++) cin >> l[i];
+  
+  sort(l.begin(), l.end());
+  reverse(l.begin(), l.end());
+  int ans = 0;
+ for (int i = 0; i < K; i++) ans += l[i];
+  cout << ans << endl;
+}

8503dcb51ea99034fbf678a89247a4a64adff6fc
NUL-git <64687067+NUL-git@users.noreply.github.com>
2021-02-17 10:49:44 +0900

    Create B - Lucas Number.cpp

---
+++ c++/Atcoder/APG4b/unfinished/3.01/B - Lucas Number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N;
+  cin >> N;
+  
+  vector<int64_t> L(N);
+  
+  for (int i = 2; i < N; i++) {
+    L.at()
+    L.at(i - 1) + L.at(i - 2)
+    
+  
+  cout << count << endl;
+  
+}

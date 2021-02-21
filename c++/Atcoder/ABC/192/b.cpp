962e0518663abec8bb035f19e24c893ffd8c18eb
NUL-git <64687067+NUL-git@users.noreply.github.com>
2021-02-21 18:04:31 +0900

    Create b.cpp

---
+++ c++/Atcoder/ABC/192/b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  string S;
+  cin >> S;
+  
+  
+  //奇数番目
+  for (int i = 0; i < S.size; i += 2) {
+    if (islower(S[i])) cout << 100 - (X % 100) << endl;
+    else cout << 100 << endl;
+
+    
+  }//if (islower(s))

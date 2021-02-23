0411254c960ad3f2ab65fc2ea74a2a6c85053e66
NUL-git <64687067+NUL-git@users.noreply.github.com>
2021-02-23 10:57:05 +0900

    Update c.cpp

--- c++/Atcoder/ABC/unfinished/192/c.cpp
+++ c++/Atcoder/ABC/unfinished/192/c.cpp
@@ -5,15 +5,17 @@ int main() {
   int N, K;
   cin >> N >> K;
   
+  string str = to_string(N);
+  
   for (int i = 0; i < K; i++) {
-    string str = to_string(N);
+    //各桁の数字大きい順
     string a = sort(str.begin(), str.end());
+    //各桁の数字大きい順
     string b = reverse(str.begin(), str.end());
     int A = stoi(a);
     int B = stoi(b);
-    
-    
-    
+    int C = B - A;
+    str = to_string(C);
   }
-  
+  cout << str << endl;
 }

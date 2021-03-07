e0f74e1cf743584936d9471123931f264bbc2ec7
NUL-git <64687067+NUL-git@users.noreply.github.com>
2021-03-07 15:49:36 +0900

    Create 22.cpp

---
+++ c++/Atcoder/APG4b/unfinished/EX/22.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N;
+  cin >> N;
+  
+  vector<int> a(N);
+  vector<int> b(N);
+  
+  for(int i = 0; i < N; i++) cin >> a.at(i) >> b.at(i);
+  
+  pair<int, int> p();
+  
+  sort(b.begin(), b.end());
+  
+  cout << p.first << " " << p.second << endl;
+  
+}

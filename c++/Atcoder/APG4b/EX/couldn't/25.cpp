2eb0637b6958081d1cf7f7eed328c59217458d6d
NUL-git <64687067+NUL-git@users.noreply.github.com>
2021-03-16 12:34:27 +0900

    Update 25.cpp

--- c++/Atcoder/APG4b/EX/couldn't/25.cpp
+++ c++/Atcoder/APG4b/EX/couldn't/25.cpp
@@ -1,3 +1,4 @@
+//自分のコード
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -80,6 +81,66 @@ int main() {
   int N;
   cin >> N;
   for (int i = 0; i < N; i++) {
+    int x;#include <bits/stdc++.h>
+using namespace std;
+
+// 各操作を行う関数を実装する
+
+// AとBに共通して含まれる要素からなる集合を返す
+bitset<50> intersection(bitset<50> A, bitset<50> B) {
+  return A & B;
+}
+// AとBのうち少なくとも一方に含まれる要素からなる集合を返す
+bitset<50> union_set(bitset<50> A, bitset<50> B) {
+  return A | B;
+}
+// AとBのうちどちらか一方にだけ含まれる要素からなる集合を返す
+bitset<50> symmetric_diff(bitset<50> A, bitset<50> B) {
+  return A ^ B;
+}
+// Aから値xを除く
+bitset<50> subtract(bitset<50> A, int x) {
+  A.set(x, 0);
+  return A;
+}
+// Aに含まれる要素に1を加える(ただし、要素49が含まれる場合は0になるものとする)
+bitset<50> increment(bitset<50> A) {
+  return A << 1;
+}
+
+// Aに含まれる要素から1を引く(ただし、要素0が含まれる場合は49になるものとする)
+bitset<50> decrement(bitset<50> A) {
+  return A >> 1;
+}
+
+// Sに値xを加える
+bitset<50> add(bitset<50> S, int x) {
+  S.set(x, 1);  // xビット目を1にする
+  return S;
+}
+
+// 集合Sの内容を昇順で出力する(スペース区切りで各要素の値を出力する)
+void print_set(bitset<50> S) {
+  vector<int> cont;
+  for (int i = 0; i < 50; i++) {
+    if (S.test(i)) {
+      cont.push_back(i);
+    }
+  }
+  for (int i = 0; i < cont.size(); i++) {
+    if (i > 0) cout << " ";
+    cout << cont.at(i);
+  }
+  cout << endl;
+}
+
+// これより下は書き換えない
+
+int main() {
+  bitset<50> A, B;
+  int N;
+  cin >> N;
+  for (int i = 0; i < N; i++) {
     int x;
     cin >> x;
     A = add(A, x);
@@ -111,4 +172,22 @@ int main() {
   } else if (com == "decrement") {
     print_set(decrement(A));
   }
+}
+
+//正解コード
+// Aに含まれる要素に1を加える(ただし、要素49が含まれる場合は0になるものとする)
+bitset<50> increment(bitset<50> A) {
+  bitset<50> ret = A << 1;  // 左シフトでまとめて+1する
+  if (A.test(49)) {
+    ret.set(0, 1);
+  }
+  return ret;
+}
+// Aに含まれる要素から1を引く(ただし、要素0が含まれる場合は49になるものとする)
+bitset<50> decrement(bitset<50> A) {
+  bitset<50> ret = A >> 1;  // 右シフトでまとめて-1する
+  if (A.test(0)) {
+    ret.set(49, 1);
+  }
+  return ret;
 }
